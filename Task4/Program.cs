708345209b92c3b9024dc6f395856da4d6997a51
Aleksandr Rytikov <9213940541@mail.ru>
2024-02-14 16:56:24 +0700

    lesson1:Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 12;
+int b = 5;
+int c = 13;
+int d = 2;
+int e = 11;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
